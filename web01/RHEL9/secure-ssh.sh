48d97f16002ce0b0d46b9c56220ab71d2ea5353e
souderton89 <hamed.mendscole@mymail.champlain.edu>
2026-02-12 13:37:35 -0500

    added a script

---
+++ web01/RHEL9/secure-ssh.sh
@@ -0,0 +1,7 @@
+#secure-ssh.sh
+#author hamed
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
+
